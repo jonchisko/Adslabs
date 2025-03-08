74f2f88334227f8c0791011bc3e7c96b1a806505
Miha <1part.awesome2parts.pie@gmail.com>
2025-03-08 18:21:33 +0100

    spawning

--- scripts/enemy_spawn.gd
+++ scripts/enemy_spawn.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 @export var enemy_scene: PackedScene
 @export var spawn_interval: float = 2.0
@@ -6,7 +6,9 @@ extends Area2D
 var screen_width
 
 func _ready():
-	screen_width = get_viewport_rect().size.x
+	await get_tree().process_frame
+	screen_width = get_window().size.x
+
 	$SpawnTimer.wait_time = spawn_interval
 	$SpawnTimer.start()
 
@@ -14,6 +16,12 @@ func _on_SpawnTimer_timeout():
 	spawn_enemy()
 
 func spawn_enemy():
-	var enemy = enemy_scene.instantiate()
-	enemy.position = Vector2(randi_range(0, screen_width), 0)
-	get_parent().add_child(enemy)
+	if enemy_scene:
+		var enemy = enemy_scene.instantiate()
+		var enemy_width = enemy.get_node("Sprite2D").texture.get_width() / 2
+		var spawn_x = clamp(randi_range(0, screen_width), enemy_width, screen_width - enemy_width)
+
+		enemy.position = Vector2(spawn_x, 0)
+		get_tree().current_scene.add_child(enemy)
+	else:
+		print("Error: enemy_scene is not assigned!")
